e6fddb9c4c76300bbfd05699d3ee7bc87043cc97
Albert Bagratunyan <bagratunyanalbert@gmail.com>
2024-01-16 00:42:44 +0400

    App Icon

---
+++ app/src/main/java/com/example/expiryalert/MainActivity.java
@@ -0,0 +1,14 @@
+ package com.example.expiryalert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
