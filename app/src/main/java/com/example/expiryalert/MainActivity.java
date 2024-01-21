f3138095d82f91abfef39a4a7f451feafb7ae31c
Albert Bagratunyan <bagratunyanalbert@gmail.com>
2024-01-21 15:46:18 +0400

    partly working fab

--- app/src/main/java/com/example/expiryalert/MainActivity.java
+++ app/src/main/java/com/example/expiryalert/MainActivity.java
@@ -3,11 +3,15 @@
 import static android.view.View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.ActivityManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
  public class MainActivity extends AppCompatActivity {
 
@@ -16,21 +20,31 @@ import android.view.Window;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         View decor = getWindow().getDecorView();
-         if (decor.getSystemUiVisibility() != SYSTEM_UI_FLAG_LIGHT_STATUS_BAR){
+         int nightModeFlags = getApplicationContext().getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
 
-         } else {
-
-         }
-
-        int nightModeFlags = getApplicationContext().getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
-
-        switch (nightModeFlags) {
-            case Configuration.UI_MODE_NIGHT_YES:
+         switch (nightModeFlags) {
+             case Configuration.UI_MODE_NIGHT_YES:
                  getWindow().getDecorView().setSystemUiVisibility(0);
                  break;
-            case Configuration.UI_MODE_NIGHT_NO:
+             case Configuration.UI_MODE_NIGHT_NO:
                  getWindow().getDecorView().setSystemUiVisibility(SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        }
+         }
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            navigateToFragment(new NewProduct());
+            fab.hide();
+        });
     }
+     private void navigateToFragment(Fragment fragment) {
+         FragmentManager fragmentManager = getSupportFragmentManager();
+         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+         fragmentTransaction.replace(R.id.home_page_activity, fragment);
+
+         fragmentTransaction.addToBackStack(null);
+
+         fragmentTransaction.commit();
+     }
 }
