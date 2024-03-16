10e7ca457bd93864a3bf9ee2178f9eb29f2f4410
Albert Bagratunyan <bagratunyanalbert@gmail.com>
2024-03-16 19:00:02 +0400

    Navigation bar

--- app/src/main/java/com/example/expiryalert/MainActivity.java
+++ app/src/main/java/com/example/expiryalert/MainActivity.java
@@ -1,50 +1,94 @@
- package com.example.expiryalert;
+package com.example.expiryalert;
 
 import static android.view.View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
 
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-import android.app.ActivityManager;
-import android.content.res.Configuration;
-import android.os.Bundle;
+import com.example.expiryalert.databinding.ActivityMainBinding;
+import com.example.expiryalert.ui.dashboard.DashboardFragment;
+import com.example.expiryalert.ui.home.HomeFragment;
+import com.example.expiryalert.ui.notifications.NotificationsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    private ActivityMainBinding binding;
 
- public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
 
-     @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-         int nightModeFlags = getApplicationContext().getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
-
-         switch (nightModeFlags) {
-             case Configuration.UI_MODE_NIGHT_YES:
-                 getWindow().getDecorView().setSystemUiVisibility(0);
-                 break;
-             case Configuration.UI_MODE_NIGHT_NO:
-                 getWindow().getDecorView().setSystemUiVisibility(SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-         }
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(view -> {
-            navigateToFragment(new NewProduct());
-            fab.hide();
+        binding.navView.setSelectedItemId(R.id.navigation_home);
+        navigateToFragment(new HomeFragment(), true);
+
+        binding.navView.setOnItemSelectedListener((BottomNavigationView.OnNavigationItemSelectedListener) item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.navigation_home) {
+                navigateToFragment(new HomeFragment(),false);
+            } else if (itemId == R.id.navigation_dashboard) {
+                navigateToFragment(new DashboardFragment(), false);
+            } else if (itemId == R.id.navigation_notifications) {
+                navigateToFragment(new NotificationsFragment(), false);
+            }
+            return true;
         });
+
+
+        int nightModeFlags = getApplicationContext()
+                .getResources()
+                .getConfiguration()
+                .uiMode & Configuration.UI_MODE_NIGHT_MASK;
+
+        switch (nightModeFlags) {
+            case Configuration.UI_MODE_NIGHT_YES:
+                getWindow().getDecorView().setSystemUiVisibility(0);
+                break;
+            case Configuration.UI_MODE_NIGHT_NO:
+                getWindow().getDecorView().setSystemUiVisibility(SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
     }
-     private void navigateToFragment(Fragment fragment) {
-         FragmentManager fragmentManager = getSupportFragmentManager();
-         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-         fragmentTransaction.replace(R.id.home_page_activity, fragment);
+    public void navigateToFragment(Fragment fragment, boolean addToBackStack) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-         fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.replace(R.id.nav_host_fragment_activity_main, fragment);
 
-         fragmentTransaction.commit();
-     }
+        fragmentTransaction.commit();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if (fragmentManager.getBackStackEntryCount() > 0) {
+            fragmentManager.popBackStack();
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
---
+++ app/src/main/java/com/example/expiryalert/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,69 @@
+package com.example.expiryalert.ui.notifications;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.expiryalert.ImageDAO;
+import com.example.expiryalert.R;
+import com.example.expiryalert.databinding.FragmentNotificationsBinding;
+
+public class NotificationsFragment extends Fragment {
+    private static final int REQUEST_CODE_PICK_IMAGE = 100;
+    private FragmentNotificationsBinding binding;
+    private ImageView imageView;
+    private Button importImageButton;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        imageView = binding.imageView;
+        importImageButton = binding.importImageButton;
+
+        ImageDAO imageDAO = new ImageDAO(requireContext()); // Use requireContext() for the fragment's context
+
+        importImageButton.setOnClickListener(v -> showImagePickerDialog());
+
+        imageDAO.open();
+
+        // Insert an example image into the database
+        Bitmap exampleImage = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_background);
+        if (exampleImage != null) {
+            imageDAO.insertImage(exampleImage);
+        }
+
+        // Retrieve and display the image from the database
+        Bitmap retrievedImage = imageDAO.retrieveImage();
+        if (retrievedImage != null) {
+            imageView.setImageBitmap(retrievedImage);
+        }
+        imageDAO.close();
+
+        return root;
+    }
+
+    private void showImagePickerDialog() {
+        Intent pickImageIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickImageIntent, REQUEST_CODE_PICK_IMAGE);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
